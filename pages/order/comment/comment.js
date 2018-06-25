612c3fddc4d4df56d626a336605ce632cac2a750
liszt <1832666492@qq.com>
2018-06-25 20:08:49 +0800

    fix-conflict

--- pages/order/comment/comment.js
+++ pages/order/comment/comment.js
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-// pages/order/comment/comment.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-  chooseicon: function (e) {
-
-    var strnumber = e.target.dataset.id;
-    var _obj = {};
-    _obj.curHdIndex = strnumber;
-    this.setData({
-      tabArr: _obj
-    });
-
-=======
 var app = getApp()
 
 Page({
@@ -85,6 +9,5 @@ Page({
     var that = this;
     this.stars = this.selectComponent("#stars");
     console.log(that.stars.getRank()+ "sdfsdf")
->>>>>>> aad2b8e15356c941bfe2b53a4815df1c2f53763f
   }
 })
