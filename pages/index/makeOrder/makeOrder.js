a323fe3ffd509ad2ee336be8f263298bed515f8b
liszt <1832666492@qq.com>
2018-06-25 14:42:48 +0800

    order-view

--- pages/index/makeOrder/makeOrder.js
+++ pages/index/makeOrder/makeOrder.js
@@ -1,107 +1,60 @@
 // pages/index/makeOrder/makeOrder.js
-Page({
+const app = getApp()
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-  
-=======
     config: {
-      base: {
-        name: {
-          title: '入住人',
-          placeholder: '名字'
-        },
-        tel: {
-          error: true,
-          title: '联系电话',
-          inputType: 'number',
-          placeholder: '请输入手机号'
-        },
-        IDNumber:{
-          title: '身份证号',
-          placeholder: '请输入身份证号'
-        },
-        roomNumber:{
-          title: '房间数',
-          stepper: 2,
-          min: 1,
-          max: 10
-        },
-        disabled: {
-          title: '发票',
-          disabled: true,
-          value: '如需发票，请到酒店前台索取'
-        }
+      name: {
+        title: '入住人',
+        placeholder: '名字'
+      },
+      tel: {
+        error: true,
+        title: '联系电话',
+        inputType: 'number',
+        placeholder: '请输入手机号'
+      },
+      IDNumber: {
+        title: '身份证号',
+        placeholder: '请输入身份证号'
+      },
+      roomNumber: {
+        title: '房间数',
+        stepper: 2,
+        min: 1,
+        max: 10
+      },
+      disabled: {
+        title: '发票',
+        disabled: true,
+        value: '如需发票，请到酒店前台索取'
       }
-    }
->>>>>>> dev-liszt
+    },
+    userInfo:null
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-<<<<<<< HEAD
-  
+  onLoad: function() {
+    var that = this
+    var userInfo=app.getUserInfo();
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      });
+      console.log(userInfo)
+    });
+    console.log("test")
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onShow: function() {
+    // console.log(this.data.config.name.placeholder)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-=======
-    
-  },
-
-  initData: function () {
-    
-  },
+  // 计数器
   handleZanStepperChange({
     detail: stepper,
     target: {
@@ -113,6 +66,5 @@ Page({
     this.setData({
       [`${componentId}.stepper`]: stepper
     });
->>>>>>> dev-liszt
   }
 })
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,26 +1,92 @@
-//logs.js
-var Bmob = require("../../utils/bmob.js");
+const app = getApp()
+
 Page({
   data: {
-    logs: []
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    tabs: {
+      list: [{
+        id: 1,
+        title: '当前订单',
+        icon: "../../static/images/icon/cart.png",
+        selected_icon: "../../static/images/icon/cart-on.png",
+        selected:true
+      }, {
+        id: 2,
+        title: '待评价',
+        icon: "../../static/images/icon/comments.png",
+        selected_icon: "../../static/images/icon/comments-on.png",
+        selected: false
+      }, {
+        id: 3,
+        title: '所有订单',
+        icon: "../../static/images/icon/all.png",
+        selected_icon: "../../static/images/icon/all-on.png",
+        selected: false
+      }]
+    },
+    selectedTab:1
+  },
+  //事件处理函数
+
+  goToRewards: function () {
+    wx.navigateTo({
+      url: './rewards/rewards',
+    })
   },
   onLoad: function () {
-    var Diary = Bmob.Object.extend("diary");
-    var diary = new Diary();
-    diary.set("title", "1");
-    diary.set("content", "2");
-    diary.set("data","3");
-    //添加数据，第一个入口参数是null
-    diary.save(null, {
-      success: function (result) {
-        // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
-        console.log("日记创建成功, objectId:" + result.id);
-      },
-      error: function (result, error) {
-        // 添加失败
-        console.log('创建日记失败');
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
       }
-    });
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  // 标签页选择
+  switchTab: function (e) {
+    var id=Number(e.currentTarget.id)
+    console.log("switchto"+id)
+    console.log(e)
+    for(var i=0;i<3;i++){
+      if(i==parseInt(id)){
+        this.data.tabs.list[i].selected=true;
+        this.data.selectedTab=i;
+      }
+      else{
+        this.data.tabs.list[i].selected = false;
+      }
+    }
+    this.setData({
+      tabs:this.data.tabs
+    })
   }
 })
